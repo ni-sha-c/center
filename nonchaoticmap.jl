3b23ff736db28c8b09c1b5679f2a2186ef62a346
Nisha Chandramoorthy <nishac@mit.edu>
2020-12-20 15:22:13 -0500

    Calculate LE to be <= 0

--- nonchaoticmap.jl
+++ nonchaoticmap.jl
@@ -23,7 +23,7 @@ function density_evolution(s)
 	x1_gr = identity_pert.(x_gr,s)
 	return x_gr, x1_gr
 end
-function plot_density_after_one_step(s)
+function plot_density_after_one_step(s,rho)
 	x, y = density_evolution(s)
 	fig = figure()
 	ax = fig.add_subplot()
@@ -32,5 +32,26 @@ function plot_density_after_one_step(s)
 	x_gr = LinRange(0.,1-1e-2,n_gr)
 	x1_gr = identity_pert.(x_gr, s)
 	dip(x) = 1.0 + s*cos(2π*x)*2π
-	ax.plot(x1_gr, 1.0./abs.(dip.(x_gr)),"o") 	
+	ax.plot(x1_gr, rho.(x_gr)/abs.(dip.(x_gr)),"o") 	
+end
+function lyapunov_exponent(s)
+	x = rand()
+	x = spinup(identity_pert, x, s, 1000)
+	le = 0.
+	n = 100000
+	dip(x) = 1.0 + s*cos(2π*x)*2π
+	for i = 1:n
+		le += log(abs(dip(x)))/n
+		x = identity_pert(x, s)
+	end
+	return le
+end
+function plot_orbit(x₀, s)
+	x = x₀
+	n = 1000
+	x = spinup(identity_pert, x, s, 1000)
+	x_orbit = run(identity_pert, x, s, 1000)
+	plot(
+
+
 end
