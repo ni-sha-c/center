7159c44f2c227bb45ac31937799f60e11dd8efc8
Nisha Chandramoorthy <nishac@mit.edu>
2020-12-20 14:51:24 -0500

    Add plot of density

--- nonchaoticmap.jl
+++ nonchaoticmap.jl
@@ -1,3 +1,4 @@
+using PyPlot
 function identity_pert(x, s)
 		return (x + s*(2 + sin(2π*x))) % 1
 end
@@ -20,9 +21,16 @@ function density_evolution(s)
 	n_gr = 10000000
 	x_gr = rand(n_gr)
 	x1_gr = identity_pert.(x_gr,s)
-	x2_gr = copy(x1_gr)
-	for i = 1:10
-		x2_gr = identity_pert.(x2_gr,s)
-	end
-	return x_gr, x1_gr, x2_gr
+	return x_gr, x1_gr
+end
+function plot_density_after_one_step(s)
+	x, y = density_evolution(s)
+	fig = figure()
+	ax = fig.add_subplot()
+	ax.hist(y, density=true, bins=25)
+	n_gr = 200
+	x_gr = LinRange(0.,1-1e-2,n_gr)
+	x1_gr = identity_pert.(x_gr, s)
+	dip(x) = 1.0 + s*cos(2π*x)*2π
+	ax.plot(x1_gr, 1.0./abs.(dip.(x_gr)),"o") 	
 end
